16b18d4fba7ed5737386f8f628e1a46ba2ce7299
Abhishek Ahirwar <abhishekahirwar1289@gmail.com>
2024-03-18 21:30:35 +0530

    Contact's backend added

--- js/script.js
+++ js/script.js
@@ -55,10 +55,64 @@ ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
 
 
 // ============= typed js =============
-const typed = new Typed('.multiple-text',{
-    strings:['FullStack Developer','BackEnd Developer','YouTuber','Trainer'],
-    typeSpeed:100,
-    backSpeed:100,
-    delaySpeed:1000,
-    loop:true
+const typed = new Typed('.multiple-text', {
+    strings: ['BackEnd Developer'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    delaySpeed: 1000,
+    loop: true
 });
+
+// ==============Contact section==============
+
+const form = document.querySelector('#contactForm');
+
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    const formData = {
+        fullName: document.getElementById('name').value,
+        contactNumber: parseInt(document.getElementById('mobileNo').value),
+        email: document.getElementById('email').value,
+        emailSubject: document.getElementById('subject').value,
+        message: document.getElementById('message').value,
+    }
+
+    // Send data using fetch API
+    fetch('https://portfoliowebsitesbackendapis.onrender.com/api/v1/contactMe', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+    })
+        .then(response => {
+            // if (!response.ok) {
+            //     throw new Error("Network response was not ok");
+            // }
+            return response.json();
+        })
+        .then(data => {
+            // console.log('Server Response:', data);
+            if (data.success) {
+                // Success: Message sent
+                alert(data.message);
+            } else {
+                // Error: Message not sent
+                alert('Message not sent. Please try again.');
+                alert(data.message);
+            }
+        })
+        .catch(error => {
+            console.error('Error:', error);
+            // Handle network errors or server errors
+            alert('An error occurred while sending the message. Please try again.');
+        });
+
+    // Clear form fields
+    document.getElementById('name').value = '';
+    document.getElementById('email').value = '';
+    document.getElementById('mobileNo').value = '';
+    document.getElementById('subject').value = '';
+    document.getElementById('message').value = '';
+}); 
